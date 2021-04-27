6c5ca16b730bd6def8f669fe1608f79ba77418ab
Lucas Buglio <lucas.buglio@gmail.com>
2021-04-26 22:52:36 -0300

    project created

---
+++ src/test/java/com/lucasbuglio/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasbuglio.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
