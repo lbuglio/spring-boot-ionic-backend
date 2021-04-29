255f9f77ea048ef85db6213bf100600152f1e4ce
Lucas Buglio <lucas.buglio@gmail.com>
2021-04-28 21:25:11 -0300

    first domain class

---
+++ src/main/java/com/lbuglio/cursomc/resources/CategoryResource.java
@@ -0,0 +1,28 @@
+package com.lbuglio.cursomc.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lbuglio.cursomc.domain.Category;
+
+@RestController
+@RequestMapping(value="/categories")
+public class CategoryResource {
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public List<Category> listar() {
+		Category cat1 = new Category(1, "TI");
+		Category cat2 = new Category(2, "Office");
+		
+		List<Category> list = new ArrayList<>();
+		list.add(cat1);
+		list.add(cat2);
+		
+		return list;
+	}
+}
+	
