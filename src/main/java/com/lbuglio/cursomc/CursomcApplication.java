b2e695809797de80bdb14e326897b6a43a7ec7ca
Lucas Buglio <lucas.buglio@gmail.com>
2021-05-10 20:49:24 -0300

    creating instantiating operation

--- src/main/java/com/lbuglio/cursomc/CursomcApplication.java
+++ src/main/java/com/lbuglio/cursomc/CursomcApplication.java
@@ -1,13 +1,30 @@
 package com.lbuglio.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.lbuglio.cursomc.domain.Category;
+import com.lbuglio.cursomc.repositories.CategoryRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Category cat1 = new Category(null, "TI");
+		Category cat2 = new Category(null, "Office");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
+	}
 }
