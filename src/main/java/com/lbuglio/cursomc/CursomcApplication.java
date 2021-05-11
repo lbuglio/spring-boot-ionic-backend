5e38697e7f88d1062c9e769d42212e654afb1cc2
Lucas Buglio <lucas.buglio@gmail.com>
2021-05-10 21:22:56 -0300

    created product repository

--- src/main/java/com/lbuglio/cursomc/CursomcApplication.java
+++ src/main/java/com/lbuglio/cursomc/CursomcApplication.java
@@ -8,14 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.lbuglio.cursomc.domain.Category;
+import com.lbuglio.cursomc.domain.Product;
 import com.lbuglio.cursomc.repositories.CategoryRepository;
+import com.lbuglio.cursomc.repositories.ProductRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 	
 	@Autowired
 	private CategoryRepository categoryRepository;
-
+	@Autowired
+	private ProductRepository productRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
@@ -25,6 +29,20 @@ public class CursomcApplication implements CommandLineRunner {
 		Category cat1 = new Category(null, "TI");
 		Category cat2 = new Category(null, "Office");
 		
+		Product p1 = new Product(null, "PC", 200.00);
+		Product p2 = new Product(null, "Printer", 80.00);
+		Product p3 = new Product(null, "Mouse", 9.00);
+		
+		cat1.getProducts().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProducts().addAll(Arrays.asList(p2));
+		
+		p1.getCategories().addAll(Arrays.asList(cat1));
+		p2.getCategories().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategories().addAll(Arrays.asList(cat1));
+		
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
+		productRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		
 	}
 }
--- src/main/java/com/lbuglio/cursomc/domain/Product.java
+++ src/main/java/com/lbuglio/cursomc/domain/Product.java
@@ -8,32 +8,32 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
 @Entity
-public class Category implements Serializable {
+public class Product implements Serializable {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
-	
-	@ManyToMany(mappedBy = "categories")
-	private List<Product> products = new ArrayList<Product>();
-	
-	public Category() {
-		// TODO Auto-generated constructor stub
-	}
+	private Double price;
 
-	public Category(Integer id, String name) {
+	@ManyToMany
+	@JoinTable(name = "PRODUCT_CATEGORY", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
+	private List<Category> categories = new ArrayList<Category>();
+
+	public Product(Integer id, String name, Double price) {
 		super();
 		this.id = id;
 		this.name = name;
+		this.price = price;
 	}
 
 	public Integer getId() {
@@ -44,22 +44,30 @@ public class Category implements Serializable {
 		this.id = id;
 	}
 
-	public String getNome() {
+	public String getName() {
 		return name;
 	}
 
-	public void setNome(String nome) {
-		this.name = nome;
+	public void setName(String name) {
+		this.name = name;
 	}
 
-	public List<Product> getProducts() {
-		return products;
+	public Double getPrice() {
+		return price;
 	}
-	
-	public void setProducts(List<Product> products) {
-		this.products = products;
+
+	public void setPrice(Double price) {
+		this.price = price;
 	}
-	
+
+	public List<Category> getCategories() {
+		return categories;
+	}
+
+	public void setCategories(List<Category> categories) {
+		this.categories = categories;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -76,7 +84,7 @@ public class Category implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Category other = (Category) obj;
+		Product other = (Product) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
