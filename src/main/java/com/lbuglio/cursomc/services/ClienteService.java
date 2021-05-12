df47b0f94793d09e1903dde8a078904cff7e73d0
Lucas Buglio <lucas.buglio@gmail.com>
2021-05-11 21:36:51 -0300

    endpoint /clientes/{id}

--- src/main/java/com/lbuglio/cursomc/services/ClienteService.java
+++ src/main/java/com/lbuglio/cursomc/services/ClienteService.java
@@ -5,20 +5,20 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.lbuglio.cursomc.domain.Categoria;
-import com.lbuglio.cursomc.repositories.CategoriaRepository;
+import com.lbuglio.cursomc.domain.Cliente;
+import com.lbuglio.cursomc.repositories.ClienteRepository;
 import com.lbuglio.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
-public class CategoriaService {
+public class ClienteService {
 	
 	@Autowired
-	private CategoriaRepository repo;
+	private ClienteRepository repo;
 
-	public Categoria buscar(Integer id) {
-		Optional<Categoria> obj = repo.findById(id);
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
 		
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
-				"Objeto nao encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+				"Objeto nao encontrado! Id: " + id + ", Cliente: " + Cliente.class.getName()));
 	}
 }
