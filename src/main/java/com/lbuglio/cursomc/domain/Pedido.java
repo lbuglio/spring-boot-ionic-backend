730d0ef1afd0e9d13e5d03f080cc9ce6aa6487ac
Lucas Buglio <lucas.buglio@gmail.com>
2021-05-26 20:55:00 -0300

    Item Pedido e ItemPedidoPK

--- src/main/java/com/lbuglio/cursomc/domain/Pedido.java
+++ src/main/java/com/lbuglio/cursomc/domain/Pedido.java
@@ -2,6 +2,8 @@ package com.lbuglio.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -10,6 +12,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 @Entity
@@ -34,6 +37,9 @@ public class Pedido implements Serializable {
 	@ManyToOne
 	@JoinColumn(name = "idEnderecoEntrega")
 	private Endereco enderecoEntrega;
+
+	@OneToMany(mappedBy = "id.pedido")
+	private Set<ItemPedido> itens = new HashSet<>();
 
 	public Pedido() {
 
@@ -85,6 +91,14 @@ public class Pedido implements Serializable {
 
 	public void setEnderecoEntrega(Endereco enderecoEntrega) {
 		this.enderecoEntrega = enderecoEntrega;
+	}
+
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
 	}
 
 	@Override
