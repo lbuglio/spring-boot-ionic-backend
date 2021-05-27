b2274c3b2b43053093e34005f929910c43e17b6c
Lucas Buglio <lucas.buglio@gmail.com>
2021-05-26 21:10:35 -0300

    endpoint /pedidos/id

--- src/main/java/com/lbuglio/cursomc/domain/PagamentoComBoleto.java
+++ src/main/java/com/lbuglio/cursomc/domain/PagamentoComBoleto.java
@@ -4,6 +4,7 @@ import java.util.Date;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.lbuglio.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
@@ -13,7 +14,9 @@ public class PagamentoComBoleto extends Pagamento {
 	 */
 	private static final long serialVersionUID = 1L;
 
+	@JsonFormat(pattern = "dd/mm/yyyy")
 	private Date dataVencimento;
+	@JsonFormat(pattern = "dd/mm/yyyy")
 	private Date dataPagamento;
 
 	public PagamentoComBoleto() {
---
+++ src/main/java/com/lbuglio/cursomc/services/PedidoService.java
@@ -0,0 +1,24 @@
+package com.lbuglio.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lbuglio.cursomc.domain.Pedido;
+import com.lbuglio.cursomc.repositories.PedidoRepository;
+import com.lbuglio.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto nao encontrado! Id: " + id + ", Pedido: " + Pedido.class.getName()));
+	}
+}
