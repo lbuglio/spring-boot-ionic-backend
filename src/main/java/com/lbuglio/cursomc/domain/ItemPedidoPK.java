d011929bfd8c3172c4683830a02d8bdae628a7d7
Lucas Buglio <lucas.buglio@gmail.com>
2021-05-26 21:20:25 -0300

    JsonIgnore

--- src/main/java/com/lbuglio/cursomc/domain/ItemPedidoPK.java
+++ src/main/java/com/lbuglio/cursomc/domain/ItemPedidoPK.java
@@ -33,6 +33,7 @@ public class ItemPedidoPK implements Serializable {
 	public void setProduto(Produto produto) {
 		this.produto = produto;
 	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
