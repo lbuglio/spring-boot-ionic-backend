13095b087ba3a8cc867a1b7d8d44897268ff40e0
Lucas Buglio <lucas.buglio@gmail.com>
2021-05-31 21:05:27 -0300

    Categorias DTO

--- src/main/java/com/lbuglio/cursomc/services/CategoriaService.java
+++ src/main/java/com/lbuglio/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.lbuglio.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,5 +44,9 @@ public class CategoriaService {
 		} catch (DataIntegrityViolationException ex) {
 			throw new DataIntegrityException("Categoria possui produtos, impossivel excluir!");
 		}
+	}
+	
+	public List<Categoria> findAll() {
+		return repo.findAll();
 	}
 }
